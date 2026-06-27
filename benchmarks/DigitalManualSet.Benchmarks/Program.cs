ddcf22e848ffb7237e0bb295db4ae811f5ded2b0
fjc-22233580 <22233580@bucks.ac.uk>
2026-06-27 19:20:21 +0100

    Update xunit

---
+++ benchmarks/DigitalManualSet.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+var config = ManualConfig
+    .Create(DefaultConfig.Instance)
+    .WithArtifactsPath("output/benchmarks");
+
+BenchmarkRunner.Run<SimpleBenchmark>(config);
+
+[MemoryDiagnoser]
+public class SimpleBenchmark
+{
+    [Benchmark]
+    public string CreateString()
+    {
+        return "Digital" + "Manual" + "Set";
+    }
+}
