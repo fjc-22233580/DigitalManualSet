2878119d2b7380475c176412811c931f13db019d
fjc-22233580 <22233580@bucks.ac.uk>
2026-06-27 19:28:58 +0100

    Added logging

--- src/DigitalManualSet.App/App.xaml.cs
+++ src/DigitalManualSet.App/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Windows;
+using Serilog;
 
 namespace DigitalManualSet.App
 {
@@ -15,12 +17,46 @@ namespace DigitalManualSet.App
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-
             // Set app theme to dark
 #pragma warning disable WPF0001
             App.Current.ThemeMode = ThemeMode.Dark;
 #pragma warning restore WPF0001
+
+            InitLogging();
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Initialise logging.
+        /// </summary>
+        private void InitLogging()
+        {
+            var logDirectory = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "DigitalManualSet",
+                "Logs");
+
+            Directory.CreateDirectory(logDirectory);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(
+                    Path.Combine(logDirectory, $"{AppInfo.ProductName}-.log"),
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 14)
+                .CreateLogger();
+
+            Log.Information("Application starting");
+
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("Application closing");
+            Log.CloseAndFlush();
+
+            base.OnExit(e);
         }
     }
 }
