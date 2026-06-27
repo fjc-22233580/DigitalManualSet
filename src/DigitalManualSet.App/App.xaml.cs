39ee26935e5f7b893e8b0d3958d928ed9fb21686
fjc-22233580 <22233580@bucks.ac.uk>
2026-06-27 18:38:32 +0100

    Added win11 fluent styles

--- src/DigitalManualSet.App/App.xaml.cs
+++ src/DigitalManualSet.App/App.xaml.cs
@@ -1,14 +1,26 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Diagnostics;
 using System.Windows;
 
-namespace App
+namespace DigitalManualSet.App
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-    }
+        public App()
+        {
+        }
 
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set app theme to dark
+#pragma warning disable WPF0001
+            App.Current.ThemeMode = ThemeMode.Dark;
+#pragma warning restore WPF0001
+        }
+    }
 }
