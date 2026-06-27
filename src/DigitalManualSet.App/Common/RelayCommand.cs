5e05326caa33c9797afb1d0bbe983921c849b25c
fjc-22233580 <22233580@bucks.ac.uk>
2026-06-27 21:25:08 +0100

    Added VM base class and a RelayCommand

---
+++ src/DigitalManualSet.App/Common/RelayCommand.cs
@@ -0,0 +1,86 @@
+﻿using System.Windows.Input;
+
+namespace DigitalManualSet.App.Common;
+
+/// <summary>
+/// Provides a reusable implementation of <see cref="ICommand"/> for binding UI actions to view model methods.
+/// </summary>
+/// <remarks>
+/// This command is intended for simple synchronous actions. It accepts an execute delegate and an optional
+/// can-execute delegate. When the command state changes, call <see cref="RaiseCanExecuteChanged"/> so WPF
+/// can re-query whether the command is currently available.
+/// </remarks>
+public sealed class RelayCommand : ICommand
+{
+    private readonly Action<object?> _execute;
+    private readonly Predicate<object?>? _canExecute;
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="RelayCommand"/> class using a parameterless action.
+    /// </summary>
+    /// <param name="execute">The action to execute when the command is invoked.</param>
+    /// <param name="canExecute">An optional function that determines whether the command can execute.</param>
+    public RelayCommand(
+        Action execute,
+        Func<bool>? canExecute = null)
+    {
+        ArgumentNullException.ThrowIfNull(execute);
+
+        _execute = _ => execute();
+        _canExecute = canExecute is null
+            ? null
+            : _ => canExecute();
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="RelayCommand"/> class.
+    /// </summary>
+    /// <param name="execute">The action to execute when the command is invoked.</param>
+    /// <param name="canExecute">
+    /// An optional predicate that determines whether the command can currently execute.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="execute"/> is <see langword="null"/>.
+    /// </exception>
+    public RelayCommand(
+        Action<object?> execute,
+        Predicate<object?>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    /// <summary>
+    /// Occurs when changes affect whether the command should execute.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged;
+
+    /// <summary>
+    /// Determines whether the command can execute using the supplied parameter.
+    /// </summary>
+    /// <param name="parameter">The command parameter supplied by the binding.</param>
+    /// <returns>
+    /// <see langword="true"/> if the command can execute; otherwise, <see langword="false"/>.
+    /// </returns>
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute?.Invoke(parameter) ?? true;
+    }
+
+    /// <summary>
+    /// Executes the command using the supplied parameter.
+    /// </summary>
+    /// <param name="parameter">The command parameter supplied by the binding.</param>
+    public void Execute(object? parameter)
+    {
+        _execute(parameter);
+    }
+
+    /// <summary>
+    /// Raises <see cref="CanExecuteChanged"/> to notify WPF that command availability may have changed.
+    /// </summary>
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ src/DigitalManualSet.App/Common/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DigitalManualSet.App.Common;
+
+
+/// <summary>
+/// Provides a base implementation of <see cref="INotifyPropertyChanged"/> for view models.
+/// </summary>
+/// <remarks>
+/// View models can inherit from this class to notify the WPF binding system when
+/// property values change. The <see cref="SetProperty{T}"/> helper updates a backing
+/// field only when the value has changed and then raises <see cref="PropertyChanged"/>.
+/// </remarks>
+public class ViewModel : INotifyPropertyChanged
+{
+    /// <summary>
+    /// Occurs when a property value has changed.
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Updates a backing field and raises <see cref="PropertyChanged"/> if the value has changed.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="backingField">A reference to the field that stores the property value.</param>
+    /// <param name="value">The new value to assign to the backing field.</param>
+    /// <param name="propertyName">
+    /// The name of the property that changed. This is supplied automatically by the compiler
+    /// when omitted by the caller.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the backing field was updated; otherwise, <see langword="false"/>
+    /// if the existing value and new value were equal.
+    /// </returns>Okay n
+    protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(backingField, value))
+        {
+            return false;
+        }
+
+        backingField = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    /// <summary>
+    /// Raises the <see cref="PropertyChanged"/> event for the specified property.
+    /// </summary>
+    /// <param name="propertyName">
+    /// The name of the property that changed. This is supplied automatically by the compiler
+    /// when omitted by the caller.
+    /// </param>
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
